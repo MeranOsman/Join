7a9335570280b506b34be1eae7f1bb98cbdcbc98
Meran Osman <meran-osman@gmx.de>
2023-11-11 23:32:19 +0100

    commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*###############################
+###
+###     JOIN APP 
+###     version 1.0.0
+###     author: unsere Namen
+###
+#################################*/
+
+/*
+*** Function for changing the checkbox image on click
+*** checked and unchecked image
+*/
+function checkboxImgChange(){
+    document.getElementById('checkbox').src = '../img/check.svg';
+}
