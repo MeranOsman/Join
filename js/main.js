832d2524b4c97bbeb65ec4e612cba5ebe1e99090
Meran Osman <meran-osman@gmx.de>
2023-11-15 18:21:48 +0100

    header menu

--- js/main.js
+++ js/main.js
@@ -10,6 +10,23 @@
 *** Function for changing the checkbox image on click
 *** checked and unchecked image
 */
-function checkboxImgChange(){
+function checkboxImgChange() {
     document.getElementById('checkbox').classList.toggle('icon-checkbox-active');
+}
+
+
+/**
+ * Function to show and close menu from header
+ */
+function showMenu() {
+    document.getElementById('menuNav').classList.toggle('display-none');
+}
+
+
+/**
+ * Function to close menu from header outside
+ * @param {*} event 
+ */
+function closeMenuOutsideClick(event) {
+
 }
