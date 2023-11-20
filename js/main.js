879967165138f0c0165e57be9605b88427542a08
Meran Osman <meran-osman@gmx.de>
2023-11-20 19:02:03 +0100

    current

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@
 #################################*/
 
 /*
-*** Function for changing the checkbox image on click
-*** checked and unchecked image
+* Function for changing the checkbox image on click
+* checked and unchecked image
 */
 function checkboxImgChange() {
     document.getElementById('checkbox').classList.toggle('icon-checkbox-active');
