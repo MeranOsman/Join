21388648d71404a4dd3e0be3a534c36a6a199a32
benjaminBennewitz <benjamin.bennewitz@gmail.com>
2023-11-22 00:12:08 +0100

    rework on contacts html

---
+++ js/addContacts.js
@@ -0,0 +1,25 @@
+/*###############################
+###
+###     JOIN APP 
+###     version 1.0.0
+###     author: unsere Namen
+###
+#################################*/
+
+
+function addContact(){
+    document.getElementById('contacts-modal').classList.remove('hide');
+    document.getElementById('modal-inner').style.setProperty('animation-direction','normal');
+}
+
+function closeContactsModal(){
+    document.getElementById('modal-inner').classList.add('move-to-right');
+    document.getElementById('modal-inner').style.setProperty('animation-direction','reverse');
+    setTimeout(hideAgain,500);
+}
+
+function hideAgain(){
+    document.getElementById('contacts-modal').classList.add('hide');
+    document.getElementById('modal-inner').classList.remove('move-to-right');
+    clearTimeout(hideAgain);
+}
