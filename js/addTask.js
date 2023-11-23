99d50a7c1f332c628781fd6a24dd2307184a8d28
Meran Osman <meran-osman@gmx.de>
2023-11-23 19:37:15 +0100

    add task

--- js/addTask.js
+++ js/addTask.js
@@ -6,8 +6,31 @@
 ###
 #################################*/
 
+/**
+ * Function opens, closes dropdown menu
+ * @param {*} event 
+ */
 function showCloseContacts(event) {
     document.getElementById('dropdownContact').classList.toggle('display-none');
     
+    event.stopPropagation();
+}
+
+/**
+ * Function opens, closes dropdown menu and changes the arrow img
+ * @param {*} event 
+ */
+function showCloseCategory(event) {
+    let dropdownCategory = document.getElementById('dropdownCategory');
+    dropdownCategory.classList.toggle('display-none');
+
+    let arrowImage = document.getElementById('arrow');
+    
+    if (arrowImage.src.endsWith('arrow_drop_down.svg')) {
+        arrowImage.src = 'img/arrow_drop_up.svg';
+    } else {
+        arrowImage.src = 'img/arrow_drop_down.svg';
+    }
+
     event.stopPropagation();
 }
