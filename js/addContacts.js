44ae86a8707f6c4b1fd5bde6a5d960992ccaae21
benjaminBennewitz <benjamin.bennewitz@gmail.com>
2023-11-23 21:20:25 +0100

    added sign up + corrections on contacts

--- js/addContacts.js
+++ js/addContacts.js
@@ -7,17 +7,28 @@
 #################################*/
 
 
+/*
+*** function to show the contacts input
+*/
 function addContact(){
     document.getElementById('contacts-modal').classList.remove('hide');
     document.getElementById('modal-inner').style.setProperty('animation-direction','normal');
 }
 
+
+/*
+*** function to close the contacts input
+*/
 function closeContactsModal(){
     document.getElementById('modal-inner').classList.add('move-to-right');
     document.getElementById('modal-inner').style.setProperty('animation-direction','reverse');
     setTimeout(hideAgain,500);
 }
 
+
+/*
+*** function to reverse the move-to-left animation
+*/
 function hideAgain(){
     document.getElementById('contacts-modal').classList.add('hide');
     document.getElementById('modal-inner').classList.remove('move-to-right');
@@ -25,8 +36,10 @@ function hideAgain(){
 }
 
 
-
+/*
+*** function to show the contacts infobox
+*/
 function infoBox(){
     document.getElementById('infobox-outer').classList.remove('hide');
-    document.getElementById('infobox-inner').style.setProperty('animation-direction','normal');
+    document.getElementById('infobox-outer').style.setProperty('animation-direction','normal');
 }
