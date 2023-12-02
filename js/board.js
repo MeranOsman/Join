9fb0a3a9947f23d23f2f6aa6c0819253e40b77ae
Meran Osman <meran-osman@gmx.de>
2023-12-03 00:04:08 +0100

    js board

---
+++ js/board.js
@@ -0,0 +1,16 @@
+let tasks = [
+    {
+        title: 'Kochwelt Page & Recipe Recommender',
+        description: 'Build start page with recipe recommendation.',
+        assignedTo: ['Anton Mayer', 'Anja Schulz', 'David Eisenberg'],
+        date: '10/05/2023',
+        prio: 'medium',
+        category: 'User Story',
+        subtasks: ['Implement Recipe recommendation', 'Start Page Layout']
+    }
+];
+
+
+function renderTasks() {
+    
+}
