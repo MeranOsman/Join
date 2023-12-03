bb13ffe81f43854bcb09c110e78d156ddf5e6f7d
Meran Osman <meran-osman@gmx.de>
2023-12-03 17:43:20 +0100

    login

--- js/login.js
+++ js/login.js
@@ -1,8 +1,23 @@
+const urlParams = new URLSearchParams(window.location.search);
+const msg = urlParams.get('msg');
+
+function login() {
+    let email = document.getElementById('mail');
+    let password = document.getElementById('pass');
+}
+
+
+if(msg) {
+    msgBox.innerHTML = msg;
+} else {
+    // display-none
+}
+
+
 /*
 *** Function for changing the checkbox image on click
 *** checked and unchecked image
 */
-
 function checkboxImgChange(){
     document.getElementById('checkbox').classList.toggle('icon-checkbox-active');
 }
---
+++ js/signUp.js
@@ -0,0 +1,25 @@
+let users = [
+    {
+        name:'Vorname Nachname',
+        email: 'meran@test.de',
+        password: 'test0000',
+        confirmPassword: 'test000'
+    }
+];
+
+
+function addUser() {
+    let name = document.getElementById('name-sign-up');
+    let email = document.getElementById('mail-sign-up');
+    let password = document.getElementById('pass-sign-up');
+    let confirmPassword = document.getElementById('pass-sign-up-2');
+
+    users.push({
+        name: name.value,
+        email: email.value,
+        password: password.value,
+        confirmPassword: confirmPassword.value
+    });
+
+    window.location.href = 'index.html?msg=Du hast dich erfolgreich registriert';
+}
