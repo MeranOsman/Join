f7203c4b83b1f9e146aacc1b0968a3f316160faa
Meran Osman <meran-osman@gmx.de>
2023-12-03 21:15:56 +0100

    init

--- js/include.js
+++ js/include.js
@@ -1,19 +1,24 @@
 async function init() {
-  await includeHTML();
-  renderAddtask();
-  renderTasks();
+  try {
+    await includeHTML();
+    renderAddtask();
+    renderTasks();
+  } catch (error) {
+    
+  }
 }
+
 
 async function includeHTML() {
   let includeElements = document.querySelectorAll('[w3-include-html]');
   for (let i = 0; i < includeElements.length; i++) {
-      const element = includeElements[i];
-      file = element.getAttribute("w3-include-html");
-      let resp = await fetch(file);
-      if (resp.ok) {
-          element.innerHTML = await resp.text();
-      } else {
-          element.innerHTML = 'Page not found';
-      }
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html");
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = 'Page not found';
+    }
   }
 }
