4ddd0a3d73ca26a0f8b52eb0a6a871c2b066ab17
benjaminBennewitz <benjamin.bennewitz@gmail.com>
2023-12-12 02:49:59 +0100

    delete unnecessary stuff

--- js/utils.js
+++ js/utils.js
@@ -16,23 +16,9 @@ let selectedCategory = [];
 let selectedColor = [];
 let priority = [];
 
+// contacts
 let bgColors = ['orange', 'vio', 'blue', 'pink', 'yell', 'azur', 'deep', 'tango'];
 
-// contacts
-let contact = [{
-    'firstName': ['Anton', 'Anja', 'Benedikt', 'David', 'Eva', 'Emmanuel', 'Marcel', 'Tatjana', 'Benjamin'],
-    'lastName': ['Mayer', 'Schulz', 'Ziegler', 'Eisenberg', 'Fischer', 'Mauer', 'Bauer', 'Wolf', 'Bennewitz'],
-    'email': ['antom@gmail.com', 'schulz@hotmail.com', 'benedikt@gmail.com', 'davidberg@gmail.com',
-        'eva@gmail.com', 'mauer@web.de', 'bauer@live.com', 'wolfi@gmx.de', 'b.benneewitz@gmail.com'
-    ],
-    'phone': ['+49 0152 123 456 789', '+49 0156 123 456 789', '+49 0172 123 456 789', '+49 0156 123 456 789',
-        '+49 0163 123 456 789', '+49 0152 123 456 789', '+49 0154 123 456 789', '+49 0157 123 456 789',
-        '+49 01522 94 315 789'
-    ],
-    'color': ['orange', 'azur', 'blue', 'pink', 'yell', 'vio', 'deep', 'tango', 'tango']
-}];
-
-// contacts NEW
 let contacts = [
     {
         'id'        : '1',
@@ -109,7 +95,7 @@ let contacts = [
 ];
 
 
-
+// board
 let tasks = [
     {
         'category'      : 'user story',
@@ -156,4 +142,3 @@ let tasks = [
         'color'         : ['orange','azur','pink']
     }
 ];
-
